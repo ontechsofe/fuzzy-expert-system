3f8f12fd1adda6dc039a2ec2399066c220cc0039
jrasmith0 <joshua.smith4@ontariotechu.net>
2020-02-17 23:46:36 -0500

    Basic skeleton setup

--- backend/routes/fuzzy.route.js
+++ backend/routes/fuzzy.route.js
@@ -1,4 +1,35 @@
 const express = require('express')
 const fuzzyRoute = express.Router()
 
+// Compatability Check
+// Main endpoint: Should be the main endpoint needed for this particular section as it should reference the functions within this module
+/* Return values:
+    - id: string
+    - name: string
+    - compatabilityWithThem: float
+    - compatabilityWithUser: float
+*/      
+fuzzyRoute.route('compatability-check').get((req, res) => {
+
+})
+
+
+
+
+// function checkUserCompatability(user) {}
+// return : value corresponding to compatability of current JWT user against passed user
+function checkUserCompatability(user) {
+
+}
+
+
+// function checkTheirCompatability(user) {}
+// return : value corresponding to compability of passed user against current JWT user
+function checkTheirCompatability(user) {
+
+}
+
+
+// function generateScale() {}
+// return : 
 
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,2 +1,43 @@
 const express = require('express')
 const userRoute = express.Router()
+
+
+// New User
+// Generate the New User into the Database
+/*
+    Return:
+        - Success: Boolean 
+*/
+
+
+
+// Login
+// Log the User into the System
+/*
+    Return:
+        - JWT (With Expiry): String
+        - Success: Boolean
+*/
+
+
+
+// Complete Profile
+// Complete the Profile Questionnaire and Log Responses into the System
+/*
+    Return:
+        - Success: Boolean
+*/
+
+
+
+// Get User Answers
+// Should return the user's answers to the questions they filled out when completing their profile
+/* 
+    Return:
+        - [{
+            Question: int
+            Response: int
+            Acceptance: int
+        }, ...]: 
+*/
+
