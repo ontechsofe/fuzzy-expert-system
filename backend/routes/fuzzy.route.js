554037429ea0895a798d6cdaf72fe3fff43b0d14
jrasmith0 <joshua.smith4@ontariotechu.net>
2020-02-18 00:07:01 -0500

    Expanded the skeleton

--- backend/routes/fuzzy.route.js
+++ backend/routes/fuzzy.route.js
@@ -6,19 +6,19 @@ const fuzzyRoute = express.Router()
 /* Return values:
     - id: string
     - name: string
-    - compatabilityWithThem: float
-    - compatabilityWithUser: float
+    - compatabilityWithThem: float => checkTheirCompatability
+    - compatabilityWithUser: float => checkUserCompatability
 */      
 fuzzyRoute.route('compatability-check').get((req, res) => {
 
 })
 
 
-
-
 // function checkUserCompatability(user) {}
 // return : value corresponding to compatability of current JWT user against passed user
 function checkUserCompatability(user) {
+    // Should pull passed user's acceptance criteria list
+    // Check current JWT user answers against passed user's acceptance criteria
 
 }
 
@@ -26,6 +26,8 @@ function checkUserCompatability(user) {
 // function checkTheirCompatability(user) {}
 // return : value corresponding to compability of passed user against current JWT user
 function checkTheirCompatability(user) {
+    // Should pull passed user's question answers list
+    // Check current passed user answers against current JWT user's acceptance criteria
 
 }
 
@@ -34,5 +36,10 @@ function checkTheirCompatability(user) {
 // return : 
 
 
+// function generateProximity () {}
+// return: Proximity closeness to corresponding answers
+function generateProximity(userOneAns, userTwoAns, scale) {
+    return (Math.abs(userOneAns - userTwoAns) / scale)
+}
 
 module.exports = fuzzyRoute
