745646ddd4f21fe5dd7e48885c422bf2e7ce05bf
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-02-18 23:18:06 -0500

    auth

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  login(username: string, password: string) {
+    this.auth.login(username, password).subscribe(data => {
+      if (data.success) {
+        localStorage.setItem('token', data.data.jwt);
+      }
+    });
   }
 
 }
--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -8,13 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   genders: string[];
-  ethnicities: string[];
 
-  constructor() {
+  constructor(private auth: AuthService) {
     this.genders = ['male', 'female', 'other'];
   }
 
   ngOnInit(): void {
+  }
+
+  register(name: string, username: string, password: string, age: string, gender: string): void {
+    console.log({name, username, password, age, gender});
+    this.auth.register(username, password, name, parseInt(age), gender).subscribe(data => {
+      if (data.success) {
+      //  REDIRECT TO LOGIN IF SUCCESS
+        console.log("User Registered")
+      } else {
+      //  ALERT USER IT DIDN'T WORK
+        console.log("User Not Registered")
+      }
+    })
   }
 
 }
