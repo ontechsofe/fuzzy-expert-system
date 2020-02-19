61b503918d4744ded03190ffa10e9b65ae1665f0
jrasmith0 <joshua.smith4@ontariotechu.net>
2020-02-19 17:43:38 -0500

    Update comma separated value list to Array

--- backend/routes/fuzzy.route.js
+++ backend/routes/fuzzy.route.js
@@ -9,23 +9,23 @@ const pool = new Pool({
     ssl: true
 })
 
-// Compatability Check
+// Compatibility Check
 // Main endpoint: Should be the main endpoint needed for this particular section as it should reference the functions within this module
 /* Return values:
     - id: string
     - name: string
-    - compatabilityWithThem: float => checkTheirCompatability
-    - compatabilityWithUser: float => checkUserCompatability
+    - compatibilityWithThem: float => checkTheirCompatibility
+    - compatibilityWithUser: float => checkUserCompatibility
 */
-fuzzyRoute.route('/compatability-check').get((req, res) => {
+fuzzyRoute.route('/compatibility-check').get((req, res) => {
     // Get all users from DB in a list format excluding current JWT user
-    // Loop over the users evaluating both checkUserCompatability and checkTheirCompatability
+    // Loop over the users evaluating both checkUserCompatibility and checkTheirCompatibility
     let preferredGender = req.body.gender
     let minAge = req.body.minAge
     let maxAge = req.body.maxAge
     let userId = req.body.userId
     
-    let usersCompatability = []
+    let usersCompatibility = []
 
     getAcceptableUsers(userId, preferredGender, minAge, maxAge).then(potentialMatches => {        
         getUser(userId).then(jwtUser => {
@@ -33,16 +33,16 @@ fuzzyRoute.route('/compatability-check').get((req, res) => {
                 potentialMatches.forEach(user => {
                     let compat = new Object()
         
-                    compat.id = user.user_id
+                    compat.userId = user.user_id
                     compat.name = user.fullname
-                    compat.compatabilityWithUser = checkUserCompatability(jwtUser, user)
-                    compat.compatabilityWithThem = checkTheirCompatability(jwtUser, user)
+                    compat.compatibilityWithUser = checkUserCompatibility(jwtUser, user)
+                    compat.compatibilityWithThem = checkTheirCompatibility(jwtUser, user)
         
-                    usersCompatability.push(compat)
+                    usersCompatibility.push(compat)
                 })
         
                 res.json({
-                    data: usersCompatability,
+                    data: usersCompatibility,
                     success: true
                 })
             } else {
@@ -56,7 +56,7 @@ fuzzyRoute.route('/compatability-check').get((req, res) => {
 })
 
 // function getAcceptableUsers(preferredGender, minAge, maxAge) {}
-// return: list of correpsonding users to loop over and check compatability with based on Gender, minAge, and maxAge
+// return: list of correpsonding users to loop over and check compatibility with based on Gender, minAge, and maxAge
 async function getAcceptableUsers(userId, preferredGender, minAge, maxAge) {
     const text = 'SELECT * FROM users WHERE complete = true AND user_id != $1 AND gender = $2 AND age >= $3 AND age <= $4'
     const values = [userId, preferredGender, minAge, maxAge]
@@ -87,13 +87,13 @@ async function getUser(userId) {
 }
 
 
-// function checkUserCompatability(user) {}
-// return : value corresponding to compatability of current JWT user against passed user
-function checkUserCompatability(jwtUser, potentialMatch) {
+// function checkUserCompatibility(user) {}
+// return : value corresponding to compatibility of current JWT user against passed user
+function checkUserCompatibility(jwtUser, potentialMatch) {
     // Should pull passed user's acceptance criteria list
     // Check current JWT user answers against passed user's acceptance criteria
-    let matchAcceptability = potentialMatch.acceptability_criteria
-    let userAnswers = jwtUser.user_responses
+    let matchAcceptability = potentialMatch.acceptability_criteria.split(',')
+    let userAnswers = jwtUser.user_responses.split(',')
 
     let proximities = []
 
@@ -102,36 +102,36 @@ function checkUserCompatability(jwtUser, potentialMatch) {
         proximities.push(generateProximity(matchAcceptability[x], userAnswers[x], 4))
     }
 
-    // Should return a value between 0 and 1 corresponding to the compatability rating of the summation of all proximities
+    // Should return a value between 0 and 1 corresponding to the compatibility rating of the summation of all proximities
     // Scaled to the number of questions that were asked and compared against
-    // Closer the number is to 0, the worse the compatability rating
-    let userCompatability = (1.00 - (proximities.reduce((a, b) => a + b, 0) / matchAcceptability.length))
+    // Closer the number is to 0, the worse the compatibility rating
+    let userCompatibility = (1.00 - (proximities.reduce((a, b) => a + b, 0) / matchAcceptability.length))
 
-    return userCompatability
+    return userCompatibility
 }
 
 
-// function checkTheirCompatability(user) {}
+// function checkTheirCompatibility(user) {}
 // return : value corresponding to compability of passed user against current JWT user
-function checkTheirCompatability(jwtUser, potentialMatch) {
+function checkTheirCompatibility(jwtUser, potentialMatch) {
     // Should pull passed user's question answers list
     // Check current passed user answers against current JWT user's acceptance criteria
-    let userAcceptability = jwtUser.acceptability_criteria
-    let matchAnswers = potentialMatch.user_responses
+    let userAcceptability = jwtUser.acceptability_criteria.split(',')
+    let matchAnswers = potentialMatch.user_responses.split(',')
 
     let proximities = []
-
+    
     // Gives a proximity list of values between 0 and 1 on a respective scale for each answer
     for (x = 0; x < userAcceptability.length; x++) {
         proximities.push(generateProximity(userAcceptability[x], matchAnswers[x], 4))
     }
 
-    // Should return a value between 0 and 1 corresponding to the compatability rating of the summation of all proximities
+    // Should return a value between 0 and 1 corresponding to the compatibility rating of the summation of all proximities
     // Scaled to the number of questions that were asked and compared against
-    // Closer the number is to 1, the worse the compatability rating
-    let theirCompatability = (1.00 - (proximities.reduce((a, b) => a + b, 0) / userAcceptability.length))
+    // Closer the number is to 1, the worse the compatibility rating
+    let theirCompatibility = (1.00 - (proximities.reduce((a, b) => a + b, 0) / userAcceptability.length))
 
-    return theirCompatability
+    return theirCompatibility
 }
 
 // function generateProximity () {}
